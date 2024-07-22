1e400eb99d282539b50f1fc14a6bc7306cb243c1
JESUS MANUEL VARELA MIRANDA <jmvarela40@misena.edu.co>
2024-07-22 16:54:10 -0500

    Cargando estructura de archivos para desplegar con vercel

---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+      primary: '#6200ee',
+      background: '#ffffff',
+      surface: '#ffffff',
+      accent: '#03dac4',
+      error: '#b00020',
+      text: '#000000',
+      onPrimary: '#ffffff',
+      onSecondary: '#000000',
+      onBackground: '#000000',
+      onSurface: '#000000',
+    },
+  };
+  
